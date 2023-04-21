b52afad297e1f644e7aead360320a49608bbfc54
DavidAR3 <64978078+DavidAR3@users.noreply.github.com>
2023-04-21 18:17:41 -0500

    Archivos de la prueba

---
+++ Prueba desarrolador web/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("header").animate({top: '30px', opacity: '1'}, "slow");
+    $(".main_logo").animate({margin: '120px auto 0px', opacity: '1'}, "slow");
+    $("header").animate({top: '30px', opacity: '1'}, "slow")
+
+    $(".col_img .img_r").animate({right: '0px'}, "slow")
+    $(".col_img .img_l").animate({left: '0px'}, "slow")
+});
+
